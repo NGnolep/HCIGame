35619999ba00d68de375e2b7af6dd11b75ed7167
NhoelGoei <157809775+NhoelGoei@users.noreply.github.com>
2024-06-25 17:57:21 +0700

    some done stage 2, maze idea
    ..

--- HCI Game/Assets/Scripts/Gate.cs
+++ HCI Game/Assets/Scripts/Gate.cs
@@ -12,7 +12,7 @@ public class Gate : MonoBehaviour
         {
             // Move the gate to an open position
             // Adjust the translation or rotation as needed for your gate
-            transform.Translate(0, 2, 0); // Example translation, adjust as needed
+            Destroy(gameObject);// Example translation, adjust as needed
             isOpen = true;
         }
     }
