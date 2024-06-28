7f87e3195ec69d04480703ee5d5a89e8c58017f7
Sightyyy <medtr333@gmail.com>
2024-06-28 17:10:25 +0700

    semoga gk error

--- HCI Game/Assets/newMainMenuHCI/Assets/MainMenu.cs
+++ HCI Game/Assets/newMainMenuHCI/Assets/MainMenu.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public void Playlevel1()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    
+    public void Playlevel2()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
